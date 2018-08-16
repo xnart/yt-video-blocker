4abea13fe26b23c5548e44b6a11980874dbe6bf9
Xnart <bekir.durak@toyotatr.com>
2018-08-16 11:34:09 +0300

    Block videos after video recommendation

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,29 +1,30 @@
 let storage = browser.storage.sync || browser.storage.local;
 
+let pageManager = document.querySelector("ytd-page-manager");
+let related = document.querySelector("#related");
+let sectionList = document.querySelector("ytd-section-list-renderer");
+let endScreen = document.querySelector(".ytp-endscreen-content");
 
-let pageManager = document.querySelector('ytd-page-manager');
-let related = document.querySelector('#related');
-let sectionList = document.querySelector('ytd-section-list-renderer');
-
-let config = {childList: true, subtree: true};
-
+let config = { childList: true, subtree: true };
 
 let observer = new MutationObserver(block);
 
 observer.observe(pageManager, config);
 observer.observe(related, config);
 observer.observe(sectionList, config);
-
+observer.observe(endScreen, config);
 
 async function block() {
-  let keywords = await storage.get('blockedKeywords');
+  let keywords = await storage.get("blockedKeywords");
 
-  let videos = document.querySelectorAll('ytd-grid-video-renderer, ytd-video-renderer, ytd-compact-video-renderer');
+  let videos = document.querySelectorAll(
+    "ytd-grid-video-renderer, ytd-video-renderer, ytd-compact-video-renderer, .ytp-videowall-still"
+  );
 
   for (const video of videos) {
     for (const keyword of keywords.blockedKeywords) {
       if (keyword.trim() === "") continue;
-      let regex = new RegExp(keyword, 'i');
+      let regex = new RegExp(keyword, "i");
       if (regex.test(video.textContent.trim())) {
         video.remove();
       }
